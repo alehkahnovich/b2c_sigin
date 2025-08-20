dcff0bcc323db57ddfe4db66cb5e068ac35da3f3
Aleh Kahnovich <53341167+alehkahnovich@users.noreply.github.com>
2025-08-20 16:34:57 +0300

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -14,7 +14,7 @@ import { LogLevel } from '@azure/msal-browser';
 export const msalConfig = {
     auth: {
         clientId: 'c67704ef-0c69-47a9-87ce-623aa4ec5894', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://adnocimpersonation.b2clogin.com/adnocimpersonation.onmicrosoft.com/B2C_1A_SIGNUP_SIGNIN', // Replace the placeholder with your tenant subdomain 
+        authority: 'https://adnocimpersonation.b2clogin.com/adnocimpersonation.onmicrosoft.com/B2C_1A_SIGNIN', // Replace the placeholder with your tenant subdomain 
         redirectUri: 'https://icy-river-0e71bd71e.1.azurestaticapps.net', // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
         postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
         knownAuthorities: ['adnocimpersonation.b2clogin.com'],
